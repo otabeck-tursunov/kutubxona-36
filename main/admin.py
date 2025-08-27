cbfc0455c31f875c1738fbff1c8376eaa5c7e963
Otabek Tursunov <otabecktursunov@gmail.com>
2025-08-27 15:04:44 +0500

    update: admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(
-    [Talaba, Muallif, Kitob, Admin, Record]
-)
+admin.site.register(Talaba)
+admin.site.register(Muallif)
+admin.site.register(Kitob)
+admin.site.register(Admin)
+admin.site.register(Record)
